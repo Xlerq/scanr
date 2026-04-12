fc201c45d86223640e48a70fa590c022385e9c5e
xlerq <xler.bull@proton.me>
2026-04-12 14:51:35 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# scanr
+
+Minimal CLI port scanner written in Rust.
+
+## Usage
+
+```bash
+scanr <ip> <start_port> [end_port]
+```
+
+## Examples
+
+```bash
+scanr 192.168.0.10 100 1000
+scanr 127.0.0.1 80
+```
+
+## Build
+
+```bash
+cargo build --release
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+fn check_len(v: &[String]) -> bool
+{
+    if v.len() > 4 
+    {
+        println!("Error: Too many arguments");
+        false
+    }
+    else if v.len() < 3
+    {
+        println!("Error: Too few arguments");
+        false
+    }
+    else {true}
+}
+
+fn check_ports(s: &u16, e: &u16) -> bool
+{
+    if s > e
+    {
+        println!("Error: Start port > End port");
+        false
+    }
+    else {true}
+}
+
+fn main()
+{
+    let args: Vec<String> = env::args().collect();
+
+    if !check_len(&args) {return;}
+
+    let ip: &str = &args[1];
+    let start: u16 = args[2].parse().unwrap();
+
+    let end: u16 = if args.len() == 4
+    {
+        args[3].parse().unwrap()
+    }
+    else {start};
+
+    if !check_ports(&start, &end) {return;}
+
+    println!("Ip: {ip}");
+    println!("Start: {start}");
+    println!("End: {end}")
 }
