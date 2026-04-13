1f28dc7a5018c0ca7b55116c91a9e8e7e45fdade
xlerq <xler.bull@proton.me>
2026-04-13 18:50:42 +0200

    iprove scan and time handling

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,78 @@
-use std::{env, net::TcpStream};
+use std::{env,
+net::
+{
+    IpAddr,
+    SocketAddr,
+    TcpStream
+},
+time::
+{
+    Duration,
+    Instant
+}};
+
 
 
 fn check_len(v: &[String]) -> bool
 {
     if v.len() > 4 
     {
-        println!("Error: Too many arguments");
+        println!("Error: too many arguments");
+        println!("Usage: scanr <ip> <start_port> [end_port]");
         false
     }
     else if v.len() < 3
     {
-        println!("Error: Too few arguments");
+        println!("Error: too few arguments");
+        println!("Usage: scanr <ip> <start_port> [end_port]");
         false
     }
     else {true}
+}
+
+fn check_and_parse_ip(ip: &str) -> Option<IpAddr>
+{
+    match ip.parse::<IpAddr>()
+    {
+        Ok(addr) => Some(addr),
+        Err(_) =>
+        {
+            eprintln!("Error: invalid socket address");
+            None
+        }
+    }
+}
+
+fn parse_port(text: &str, field: &str) -> Option<u16>
+{
+    match text.parse()
+    {
+        Ok(port) => Some(port),
+        Err(_) =>
+        {
+            eprintln!("Error {field} port must be a number");
+            None
+        }
+    }
 }
 
 fn check_ports(s: &u16, e: &u16) -> bool
 {
     if s > e
     {
-        println!("Error: Start port > End port");
+        println!("Error: start_port cannot be greater than end_port");
         false
     }
     else {true}
 }
 
-fn scan_port(ip: &str, port: u16) -> bool
+fn scan_port(ip_port: &SocketAddr) -> bool
 {
-    TcpStream::connect((ip, port)).is_ok()
+    
+    let timeout: Duration = Duration::from_millis(500);
+    TcpStream::connect_timeout(ip_port, timeout).is_ok()
 }
+
 
 fn main()
 {
@@ -37,27 +80,25 @@ fn main()
 
     if !check_len(&args) {return;}
 
-    let ip: &str = &args[1];
-    let start: u16 = match args[2].parse()
+    let ip: IpAddr = match check_and_parse_ip(&args[1])
     {
-        Ok(port) => port,
-        Err(_) =>
-        {
-            eprintln!("Error: start port must be a number");
-            return;
-        }
+        Some(ip) => ip,
+        None => return,   
     };
+
     
+    let start: u16 = match parse_port(&args[2], "start")
+    {
+        Some(port) => port,
+        None => return,
+    };
+
     let end: u16 = if args.len() == 4
     {
-        match args[3].parse()
+        match parse_port(&args[3], "end")
         {
-            Ok(port) => port,
-            Err(_) =>
-            {
-                eprintln!("Error: start port must be a number");
-                return;
-            }
+            Some(port) => port,
+            None => return,
         }
     }
     else {start};
@@ -65,14 +106,22 @@ fn main()
     if !check_ports(&start, &end) {return;}
     
     let mut open_count: u16 = u16::MIN;
+    let timer: Instant = Instant::now();
+
     for i in start..=end
     {
-        if scan_port(ip, i)
+        let ip_port: SocketAddr = SocketAddr::new(ip, i);
+        if scan_port(&ip_port)
         {
             println!("{i} is open");
             open_count += 1;
         }
     }
 
-    if open_count == 0 {println!("No open ports found")}
+    let elapsed: Duration = timer.elapsed();
+
+    if open_count == 0 {println!("No open ports found")};
+
+    println!("Elapsed: {} s", elapsed.as_secs_f32());
+    
 }
