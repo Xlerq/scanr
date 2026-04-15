22fa8eb671b7c1b18c95a08e4309416398300e5d
xlerq <xler.bull@proton.me>
2026-04-15 18:05:22 +0200

    better code structure

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,12 @@
-use std::{env,
-net::
-{
-    IpAddr,
-    SocketAddr,
-    TcpStream
-},
-time::
-{
-    Duration,
-    Instant
-}};
-
-
+use std::{
+    env,
+    net::{IpAddr, SocketAddr, TcpStream},
+    time::{Duration, Instant},
+};
 
 fn check_len(v: &[String]) -> bool
 {
-    if v.len() > 4 
+    if v.len() > 4
     {
         println!("Error: too many arguments");
         println!("Usage: scanr <ip> <start_port> [end_port]");
@@ -27,7 +18,10 @@ fn check_len(v: &[String]) -> bool
         println!("Usage: scanr <ip> <start_port> [end_port]");
         false
     }
-    else {true}
+    else
+    {
+        true
+    }
 }
 
 fn check_and_parse_ip(ip: &str) -> Option<IpAddr>
@@ -63,30 +57,85 @@ fn check_ports(s: &u16, e: &u16) -> bool
         println!("Error: start_port cannot be greater than end_port");
         false
     }
-    else {true}
+    else
+    {
+        true
+    }
 }
 
 fn scan_port(ip_port: &SocketAddr) -> bool
 {
-    
     let timeout: Duration = Duration::from_millis(500);
     TcpStream::connect_timeout(ip_port, timeout).is_ok()
 }
 
+fn scan_range(ip: IpAddr, start: u16, end: u16) -> Vec<u16>
+{
+    let mut open_ports: Vec<u16> = Vec::new();
+    for i in start..=end
+    {
+        let ip_port: SocketAddr = SocketAddr::new(ip, i);
+        if scan_port(&ip_port)
+        {
+            open_ports.push(i);
+        }
+    }
+    open_ports
+}
+
+fn print_summary(
+    open_ports: &Vec<u16>,
+    elapsed_time: &Duration,
+    ip: &IpAddr,
+    start: &u16,
+    end: &u16,
+)
+{
+    if open_ports.is_empty()
+    {
+        print!("No open ports found");
+    }
+    else
+    {
+        println!("Open ports");
+        println!("=============");
+
+        for open in open_ports
+        {
+            print!("{open} ");
+        }
+    }
+
+    println!("\n=============");
+    println!("Ip: {ip}");
+
+    if start != end
+    {
+        println!("Scan range: {start} - {end}");
+    }
+    else
+    {
+        println!("Scan port: {start}");
+    }
+
+    println!("Elapsed: {} s", elapsed_time.as_secs_f32());
+}
 
 fn main()
 {
     let args: Vec<String> = env::args().collect();
 
-    if !check_len(&args) {return;}
+    if !check_len(&args)
+    {
+        return;
+    }
 
     let ip: IpAddr = match check_and_parse_ip(&args[1])
     {
         Some(ip) => ip,
-        None => return,   
+        None => return,
     };
 
-    
     let start: u16 = match parse_port(&args[2], "start")
     {
         Some(port) => port,
@@ -101,29 +150,20 @@ fn main()
             None => return,
         }
     }
-    else {start};
-
-    if !check_ports(&start, &end) {return;}
-    
-    let mut open_count: usize = usize::MIN;
-    let timer: Instant = Instant::now();
-
-    for i in start..=end
+    else
     {
-        let ip_port: SocketAddr = SocketAddr::new(ip, i);
-        if scan_port(&ip_port)
-        {
-            println!("{i} is open");
-            open_count += 1;
-        }
+        start
+    };
+
+    if !check_ports(&start, &end)
+    {
+        return;
     }
 
+    let timer: Instant = Instant::now();
+
+    let open_ports: Vec<u16> = scan_range(ip, start, end);
+
     let elapsed: Duration = timer.elapsed();
-
-    if open_count == 0 {println!("No open ports found")};
-
-    println!("Target: {ip}
-    Scanned ports: {start}-{end}
-    Open ports found: {open_count} 
-    Elapsed: {}s", elapsed.as_secs_f32());
+    print_summary(&open_ports, &elapsed, &ip, &start, &end);
 }
