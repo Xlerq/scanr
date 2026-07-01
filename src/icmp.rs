24f774ffca24ebe06b508f52ee3ebf7c72679aeb
xlerq <xler.bull@proton.me>
2026-07-01 17:12:50 +0200

    add icmo todo

---
+++ src/icmp.rs
@@ -0,0 +1,14 @@
+use std::net::IpAddr;
+use std::time::Duration;
+
+fn checksum(data: [u8]) -> u16 {
+    todo!()
+}
+
+fn build_echo_request(id: u16, seq: u16) -> Vec<u16> {
+    todo!()
+}
+
+fn ping_host(ip: IpAddr, timeout: Duration) -> bool {
+    todo!()
+}
